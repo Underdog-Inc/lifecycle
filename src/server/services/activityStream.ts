d8a2c6cff6959b68ff0105f17a5c3e6beae7b171
lucien-UD <lucien.minot@underdogfantasy.com>
2025-06-10 12:36:41 -0700

    updated comments to show build info

--- src/server/services/activityStream.ts
+++ src/server/services/activityStream.ts
@@ -404,12 +404,12 @@ export default class ActivityStream extends BaseService {
     const isStatic = build?.isStatic ?? false;
     const enabledFeatures = build?.enabledFeatures || [];
     const labels = pullRequest?.labels || [];
-    const hasUseDeprecatedStatusComment = labels?.includes(Labels.ENABLE_LIFECYCLE_STATUS_COMMENTS);
+    const hasDisabledLifecycleBuildComments = labels?.includes(Labels.DISABLE_BUILD_COMMENTS);
     const hasGithubStatusCommentEnabled = enabledFeatures.includes('hasGithubStatusComment');
     const isDeployed = build?.status === BuildStatus.DEPLOYED;
     const hasPurgeFastlyServiceCachLabel = labels?.includes(Labels.PURGE_FASTLY_SERVICE_CACHE);
     const isPurgingFastlyServiceCache = hasPurgeFastlyServiceCachLabel && isDeployed;
-    const isShowingStatusComment = isStatic || hasUseDeprecatedStatusComment || hasGithubStatusCommentEnabled;
+    const isShowingStatusComment = (isStatic || hasGithubStatusCommentEnabled) && !hasDisabledLifecycleBuildComments;
     if (!buildId) {
       logger.error(`${prefix}[buidIdError] No build ID found ${suffix}`);
       throw new Error('No build ID found for this build!');
@@ -475,7 +475,7 @@ export default class ActivityStream extends BaseService {
    */
   private async editCommentForBuild(build: Build, deploys: Deploy[]) {
     let message = ``;
-    const enableLifecycleStatusComments = `Add \`${Labels.ENABLE_LIFECYCLE_STATUS_COMMENTS}\``;
+    const disableLifecycleBuildComments = `Add \`${Labels.DISABLE_BUILD_COMMENTS}\``;
     message += `## ✏️ Environment Overrides\n`;
     message += '<details>\n';
     message += '<summary>Usage</summary>\n\n';
@@ -483,7 +483,7 @@ export default class ActivityStream extends BaseService {
     const enabledFeatures = build?.enabledFeatures || [];
     const hasEnabledFeatures = enabledFeatures?.length > 0;
     if (hasEnabledFeatures) message += `* LC testing features: ${enabledFeatures.join(', ')}\n`;
-    message += `* To enable status comments, add the ${enableLifecycleStatusComments} label.\n`;
+    message += `* Build status comments are enabled by default. To disable status comments, add the ${disableLifecycleBuildComments} label.\n`;
     message += `* You can enable/disable individual service by clicking the Checkboxes below, OR\nEditing this comment to Enable/Disable multiple services at the same time by changing between \`[]\` and \`[X]\`.\n`;
     message += `* You can also edit the branch name or URL of an external service, to further customize your deployment.\n\n`;
     message += '</details>\n\n';
